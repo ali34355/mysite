73057fe458558d2cea3c8c1569323f6b58deb5af
ali34355 <farahiamirali458@gmail.com>
2026-06-18 21:01:35 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+
+app.get('/', (req, res) => {
+  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  const time = new Date().toLocaleString();
+
+  fs.appendFileSync('visitors.txt', `IP: ${ip} | Time: ${time}\n`);
+
+  res.send(`<h1>Your IP is: ${ip}</h1>`);
+});
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
